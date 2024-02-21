14e6ac1be8950859456bc47e005a8e31c7605174
AtharvaBorawake <Atharvaborawake210@gmail.com>
2024-02-21 21:18:28 +0530

    first commit

---
+++ src/app/restaurent-dash/restaurent-dash.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder  } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import {RestaurentData} from './restaurent.model';
+import { max } from 'rxjs';
+
+
+@Component({
+  selector: 'app-restaurent-dash',
+  templateUrl: './restaurent-dash.component.html',
+  styleUrls: ['./restaurent-dash.component.css']
+})
+
+export class RestaurentDashComponent implements OnInit {
+
+  formValue!:FormGroup
+  restaurentModelObj : RestaurentData = new RestaurentData;
+  allRestaurentData: any;
+  showAdd!:boolean;
+  showBtn!:boolean;
+  maxid: number=0;
+  obj: any;
+  constructor(private formbuilder: FormBuilder, private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      name: [''],
+      email: [''],
+      mobile: [''],
+      address: [''],
+      services: [''],
+    })
+    this.getAllData();
+  }
+
+  clickAddResto()
+  {
+    this.formValue.reset();
+    this.showAdd = true;
+    this.showBtn = true;
+  }
+ 
+  addRestaurent()
+  {
+    this.getAllData();
+    console.log("MAX ID :"+ this.maxid);
+    if(this.maxid==null || this.maxid==-Infinity)
+      {
+        console.log("max id is null"+ this.maxid)
+        this.maxid=0;
+      }
+    this.restaurentModelObj.id= (this.maxid+1).toString();
+    console.log("MAX ID : IN ADD REST"+ this.maxid+1)
+    this.restaurentModelObj.name = this.formValue.value.name;
+    this.restaurentModelObj.email = this.formValue.value.email;
+    this.restaurentModelObj.mobile = this.formValue.value.mobile;
+    this.restaurentModelObj.address = this.formValue.value.address;
+    this.restaurentModelObj.services = this.formValue.value.services;
+
+    this.api.postRestaurent(this.restaurentModelObj).subscribe(res => {
+      console.log(res);
+      alert("Restaurent Added Successfully");
+      this.formValue.reset();
+
+      let ref= document.getElementById('close');
+      ref?.click();
+
+      this.getAllData();
+
+    }, err=>{
+      console.log(err);
+      alert("Restaurent Added Failed!");
+    })
+  }
+
+  getAllData()
+  {    
+    this.api.getAllRestaurents().subscribe(res => {
+      console.log("all rest :"+ JSON.stringify(res));    
+      this.maxid= Math.max.apply(Math, res.map(function(obj: any) { return obj.id; }))
+      console.log("maxid :"+ this.maxid);
+      this.allRestaurentData= res;
+    }, err=>{
+      console.log(err);
+    })
+  }
+
+  deleteResto(data: any)
+  {
+    this.api.deleteRestaurant(data).subscribe((res: any) => {
+      console.log("data: "+res);
+      alert("Restaurent Deleted Successfully");
+      this.getAllData();
+    })
+  }
+
+  onEditResto(data: any)
+  {
+    this.showAdd = false;
+    this.showBtn = true;
+    
+    this.restaurentModelObj.id = data.id;
+    console.log("data id: "+data.id);
+    this.formValue.controls['name'].setValue(data.name);
+    this.formValue.controls['email'].setValue(data.email);
+    this.formValue.controls['mobile'].setValue(data.mobile);
+    this.formValue.controls['address'].setValue(data.address);
+    this.formValue.controls['services'].setValue(data.services);
+
+ 
+  }
+  updateResto(){
+    this.restaurentModelObj.name = this.formValue.value.name;
+    this.restaurentModelObj.email = this.formValue.value.email;
+    this.restaurentModelObj.mobile = this.formValue.value.mobile;
+    this.restaurentModelObj.address = this.formValue.value.address;
+    this.restaurentModelObj.services = this.formValue.value.services;
+    console.log("restaurentModelObj.id: "+this.restaurentModelObj.id);
+    console.log("this.restaurentModelObj: "+this.restaurentModelObj);
+    this.api.updateRestaurant(this.restaurentModelObj.id,this.restaurentModelObj).subscribe((res: any) => {
+      alert("Restaurent Updated Successfully");
+      this.formValue.reset();
+
+      let ref= document.getElementById('close');
+      ref?.click();
+
+      this.getAllData();
+
+    })
+    
+  }
+
+  
+}
